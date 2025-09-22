b9a1bfdaa2e141037567769696bd4f678e492116
vardaans60-star <vardaans60@gmail.com>
2025-09-22 16:50:05 +0530

    Create Q7.c

---
+++ Q7.c
@@ -0,0 +1,15 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include<stdio.h>
+int main(){
+	int x,a,b=0;
+	printf("Choose your numbers for a and b:\n");
+	scanf("%d %d",&a,&b);
+	x=(a>b)?a:b;
+	printf("%d is the bigger of two numbers",x);
+	
+}
