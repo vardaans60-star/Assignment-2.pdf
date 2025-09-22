e22da95f25a1f2b22458846a6813d8cf68d1cb60
vardaans60-star <vardaans60@gmail.com>
2025-09-22 16:01:09 +0530

    Create Q4.c

---
+++ Q4.c
@@ -0,0 +1,30 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a;
+	a=4;
+printf("Enter two integer values a\n ");
+scanf("%d",&a);
+a+=2;
+printf("The value of above expression is:%d\n,a");
+a-=3;
+printf("The value of above expression is:%d\n",a);
+a*=4;
+printf("The value of above expression is:%d\n",a);
+a/=2;
+printf("The value of above expression is:%d\n",a);
+a%=4;
+printf("The value of above expression is:%d\n",a);
+
+
+
+
+	
+}
