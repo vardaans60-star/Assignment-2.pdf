cbc034e1e97be395db24ad74d541c5808bc86203
vardaans60-star <vardaans60@gmail.com>
2025-09-22 15:59:43 +0530

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,21 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int x;
+	x=12;
+	printf("Your chosen value is %dvardaan\n",x);
+	int y=1;
+	y=18;
+	printf("Your chosen value is %d\tvardaan\n",y);
+	int z;
+	z=111;
+	printf("Your chosen value is %d\bvardaan",x);
+	return 0;
+}
