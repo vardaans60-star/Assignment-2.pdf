dc243aeee5d819a4e2f5afc388f2ad5e7a2496cc
vardaans60-star <vardaans60@gmail.com>
2025-09-22 19:58:27 +0530

    Create Q9.c

---
+++ Q9.c
@@ -0,0 +1,18 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include<stdio.h>
+int main()
+{
+	float x,sum,y,n,mean;
+	printf("Enter your numbers:");
+	scanf("%f %f",&x,&y);
+	sum=x+y;
+	n=2;
+	mean=sum/n;
+	printf("Your mean is %.2f",mean);
+	
+}
