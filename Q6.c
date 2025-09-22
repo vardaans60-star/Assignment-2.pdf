02b593b74d69683071e67fdbc74cc260fbefbea0
vardaans60-star <vardaans60@gmail.com>
2025-09-22 16:49:08 +0530

    Create Q6.c

---
+++ Q6.c
@@ -0,0 +1,21 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include<stdio.h>
+int main(){
+	int x,shiftleft,shiftright,y;
+	printf("Enter your number for x:");
+	scanf("%d",&x);
+	shiftleft=x<<4;
+	printf("%d is your updated number\n",shiftleft);
+	printf("Enter your number for y:");
+	scanf("%d",&y);
+	shiftright=y>>4;
+	printf("%d is your updated number",shiftright);
+	
+	
+	
+}
