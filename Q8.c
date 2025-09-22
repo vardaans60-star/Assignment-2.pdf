5bede766a71cf49518fbbff078774d6b21991840
vardaans60-star <vardaans60@gmail.com>
2025-09-22 19:56:43 +0530

    Create Q8.c

---
+++ Q8.c
@@ -0,0 +1,22 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include<stdio.h>
+int main()
+{
+	int b,x;
+	char c='q';
+	float y;
+    double z;
+	long int a;
+	b=sizeof(x);
+	printf("The size of int is %d\n",b);
+    printf("The size of char is %d\n",(int)sizeof(c));
+    printf("The size of float is %d\n",sizeof(y));
+    printf("The size of double is %d\n",sizeof(z));
+    printf("The size of long int is %d\n",sizeof(a));
+    
+}
