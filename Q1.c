60107865d432784963354117ee74d11bdf47ff0f
vardaans60-star <vardaans60@gmail.com>
2025-09-22 15:57:10 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,23 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float a=1.57,b=0,c=0.78,e=4,v=16;
+	double x= sin(a);
+	printf("The value of function sin is:%lf\n",x);
+   double y=cos(b);
+	printf("The value of function cos is:%lf\n",y);
+	double z=log(c);
+	printf("The value of function log is:%lf\n",z);
+	float d=pow(e,2);
+	printf("The value of function square is:%f\n",d);
+	float u=sqrt(v);
+	printf("The value of function root is:%f\n",u);
+
+}
